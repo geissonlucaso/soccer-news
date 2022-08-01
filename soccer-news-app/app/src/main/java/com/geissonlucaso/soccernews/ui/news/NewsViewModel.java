b90e2e4bdeeb77726e87028424c173c4b4df3194
Geisson de Oliveira <geissonlucaso@gmail.com>
2022-08-01 14:22:48 -0300

    Create the NewsAdapter and test the RecyclerView.

--- soccer-news-app/app/src/main/java/com/geissonlucaso/soccernews/ui/news/NewsViewModel.java
+++ soccer-news-app/app/src/main/java/com/geissonlucaso/soccernews/ui/news/NewsViewModel.java
@@ -4,16 +4,29 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.geissonlucaso.soccernews.domain.News;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<List<News>> news;
 
     public NewsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is news fragment");
+        news = new MutableLiveData<>();
+
+        // TODO Remove mock news.
+        List<News> listNews = new ArrayList<>();
+        listNews.add(new News("Cruzeiro é Campeão Antecipado.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "));
+        listNews.add(new News("Framengo Empata em 0x0.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "));
+        listNews.add(new News("Corinthians é Derrotado por 2x0.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "));
+        listNews.add(new News("Copa do Mundo Feminina Será no Brasil.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "));
+
+        news.setValue(listNews);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<News>> getNews() {
+        return news;
     }
 }
